10add377ec4161a6cff96397ed2c6aba9ed8bfc8
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-05-18 22:43:54 -0500

    Rendered the data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,22 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [adjectives, setAdjectives] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api'); //
+      const data = await response.json();
+      setAdjectives(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div className="gameBoard" id="gameBoard"></div>;
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 const db = require('../model/helper');
 
-/* GET home page. */
 router.get('/', async (_, res) => {
-  const results = await db('SELECT * FROM adjectives;');
-  res.send(results.data);
+  try {
+    const results = await db('SELECT * FROM adjectives;');
+    res.send(results.data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
