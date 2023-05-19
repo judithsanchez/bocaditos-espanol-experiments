3736c5dcbe69909845932e8392a07b652cb0ed9e
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-05-18 21:22:13 -0500

    Made the database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
 
 function App() {
-
-  return (
-  
-  )
+  return <div className="gameBoard" id="gameBoard"></div>;
 }
 
-export default App
+export default App;
