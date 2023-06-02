79399e8b113a93b88b4c011994a09f210abf7939
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-06-02 14:00:07 -0500

    Added new game button

--- client/src/components/GameFinished.jsx
+++ client/src/components/GameFinished.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import './GameFinished.css';
 
 function GameFinished() {
+  const handleReload = () => {
+    window.location.reload(); // Reload the page
+  };
+
   return (
     <div className="gameFinished">
       <img
         src="https://cdn.bfldr.com/Z0BJ31FP/at/43s6h5f79pc55mk9fc4gwg7r/bruja-game-won.svg"
         alt="Won Game"
       />
-      <p>I suppose you won...</p>
+      <div className="textButton">
+        <p>I suppose you won...</p>
+        <button onClick={handleReload}>¡Otra vez!</button>
+      </div>
     </div>
   );
 }
