480ca659c398172fca862b031774a3b72f3767a3
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-06-02 13:00:38 -0500

    Design of the game finished component

--- client/src/components/GameFinished.jsx
+++ client/src/components/GameFinished.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './GameFinished.css';
 
 function GameFinished() {
   return (
     <div className="gameFinished">
-      <p>Game won</p>
+      <img
+        src="https://cdn.bfldr.com/Z0BJ31FP/at/43s6h5f79pc55mk9fc4gwg7r/bruja-game-won.svg"
+        alt="Won Game"
+      />
+      <p>I suppose you won...</p>
     </div>
   );
 }
